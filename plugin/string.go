318ae070382200c58fbcd2285f559be2430f3bc4
Brian Clapper <brian.clapper@swiftcomply.com>
2019-09-24 16:34:11 +0000

    Change generated code for not_empty_string
    We could have not_empty_string and then other validation options,
    so only do them if we have a value
    
    Also, its possible you want to allow either an empty string or some
    other option with validates, so this I think will allow that, maybe

--- plugin/string.go
+++ plugin/string.go
@@ -17,10 +17,10 @@ func (p *Plugin) generateStringValidationCode(fieldName string, fieldValue strin
 	if v.Uc != nil && *v.Uc {
 		p.P(`%s = %s.ToUpper(%s)`, fieldValue, p.stringsPkg.Use(), fieldValue)
 	}
-	if v.NotEmptyString != nil && *v.NotEmptyString {
-		p.P(`if %s == "" {`, fieldValue)
-		p.generateErrorCode(fieldName, "", "{field} can not be an empty string", v, mv, field, "")
-		p.P(`}`)
+	if v.NotEmptyString != nil {
+		// start this now, we will close it out later, if we've set not_empty_string there is no sense doing extra validation on it it if is one
+		// so this begins the wrap of the other possible validations
+		p.P(`if %s != "" {`, fieldValue)
 	}
 	if v.Matches != nil {
 		p.P(`if %s != "%s" {`, fieldValue, v.GetMatches())
@@ -60,6 +60,16 @@ func (p *Plugin) generateStringValidationCode(fieldName string, fieldValue strin
 	if v.IsEmail != nil && *v.IsEmail {
 		p.P(`if !isValidEmail(%s) {`, fieldValue)
 		p.generateErrorCode(fieldName, "", "{field} must be a valid email address", v, mv, field, "")
+		p.P(`}`)
+	}
+
+	// so if we set not_empty_string, we only did validation if we had some value, now we need to decide if we need to output an error that it was empty
+	if v.NotEmptyString != nil {
+		if *v.NotEmptyString {
+			p.P(`} else {`)
+			p.generateErrorCode(fieldName, "", "{field} can not be an empty string", v, mv, field, "")
+		}
+		// we opened these up at the start, so close them now
 		p.P(`}`)
 	}
 }
